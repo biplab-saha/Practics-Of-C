a13db0c8a695ea963765ddda38b57a8152706d0a
Ashish Kumar <ashishh2012@outlook.com>
2025-04-10 21:22:00 +0530

    ashishcode

---
+++ eight.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+      int a= 98;
+      char ch='c';
+      printf("%c,%d\n",a,ch);
+      return 0;
+
+
+}
---
+++ tenth.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  printf("%10s\n","india");
+  printf("%4S\n","india");
+  printf("%2s\n","india");
+  printf("%.2s\n","india");
+printf("%5.2s\n","india");
+return 0;
+}
