64f4d435aa2dcd4e9b08765c5f1d566eb0241cd3
Biplab Saha <biplab5523x@gmil.com>
2025-04-13 18:43:56 +0530

    star pattern

--- printStarLine.c
+++ printStarLine.c
@@ -1,7 +1,17 @@
-#include<stdio.h>
-int main(){
-    int a,b;
-    for (a=0;a<=5;a++){
-        printf("*",a);
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("enter the value of star: ");
+    scanf("%d", &a);
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
     }
+    return 0;
 }
