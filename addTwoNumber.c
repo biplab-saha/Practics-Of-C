191157b26c79444c0cf4f88b06cace63a6d6eae8
Biplab Saha <biplab5523x@gmil.com>
2025-04-14 23:17:11 +0530

    new code

---
+++ addTwoNumber.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a ;
+    int b = 7;
+    char type;
+
+    // printf("Enter the number:");
+    // scanf("%d",&a);
+    // printf("%d",a);// intiger jonno %d value print korar jonno variable name nicho...
+    // printf("%d",&a);
+
+    // clarser();
+    return 0;
+}
--- fifth.c
+++ fifth.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main()
 {
+  int ch;
   printf("enter a character");
   scanf("%c",&ch);
   printf("%d\n",ch);
